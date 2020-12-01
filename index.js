b1c0884f00221a3024da12b7eafe37b350f08a89
“saschakala” <mxsaschakala@gmail.com>
2020-12-01 13:47:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, name){ 
+    return drivers.filter(driver =>
+        driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => 
+        driver.split("")
+        .splice(0, string.length)
+        .join("") === string)
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver =>
+        driver["name"] === name)
+}
